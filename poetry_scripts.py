0fafe37d98567fbc24a8f58fc41e7fa9d21802f2
Atticuszz <1831768457@qq.com>
2024-02-06 14:00:44 +0800

    run  .pre-commit-config.yaml

--- poetry_scripts.py
+++ poetry_scripts.py
@@ -2,32 +2,41 @@ import subprocess
 
 
 def run_cmd(cmd: str, cwd: str = None) -> None:
-    subprocess.run(cmd, shell=True, check=True, cwd=cwd)
+    try:
+        subprocess.run(cmd, shell=True, check=True, cwd=cwd)
+    except subprocess.CalledProcessError as e:
+        print(f"Command '{e.cmd}' in {cwd} failed with exit status {e.returncode}")
 
 
-def run_pre_commit_in_submodules() -> None:
-    # 获取所有子模块路径
-    cmd = "git config --file .gitmodules --get-regexp path | awk '{ print $2 }'"
+def get_submodule_paths() -> list[str]:
+    cmd = "git config --file .gitmodules --get-regexp path"
     result = subprocess.run(
         cmd, shell=True, check=True, stdout=subprocess.PIPE, text=True
     )
-    submodules = result.stdout.strip().split("\n")
+    paths = [line.split()[-1] for line in result.stdout.split("\n") if line]
+    return paths
 
-    for submodule in submodules:
-        print(f"Running pre-commit in {submodule}")
-        # 进入子模块目录并执行pre-commit
-        run_cmd("poetry run pre-commit run --all-files", cwd=submodule)
+
+def run_pre_commit_in_submodules() -> None:
+    submodules = get_submodule_paths()
+    for path in submodules:
+        print(f"Running pre-commit in {path}")
+        run_cmd("pre-commit run --all-files", cwd=path)
 
 
 def run_tests() -> None:
     # Install requirements by poetry
     run_cmd("poetry install")
-
-    # Run pre-commit tests in the main repository
-    # run_cmd("poetry run pre-commit run --all-files")
+    # Run pre-commit
+    # run_cmd("poetry run pre-commit autoupdate")
+    # run_cmd("poetry run pre-commit clean")
+    # run_cmd("poetry run pre-commit install")
 
     # Optionally run pre-commit in all submodules
     run_pre_commit_in_submodules()
+
+    # Run pre-commit tests in the main repository
+    run_cmd("poetry run pre-commit run --all-files")
 
     # Generate coverage report
     # run_cmd("poetry run pytest --cov=./ --cov-report=xml --cov-report=html -vv")
