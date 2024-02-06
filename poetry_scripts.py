16f1f145a725050501160c8e191fd23ace2d5905
Atticuszz <1831768457@qq.com>
2024-02-06 13:17:13 +0800

    add  .pre-commit-config.yaml

--- poetry_scripts.py
+++ poetry_scripts.py
@@ -1,22 +1,36 @@
 import subprocess
 
 
-def run_cmd(cmd: str) -> None:
-    subprocess.run(cmd, shell=True, check=True)
+def run_cmd(cmd: str, cwd: str = None) -> None:
+    subprocess.run(cmd, shell=True, check=True, cwd=cwd)
+
+
+def run_pre_commit_in_submodules() -> None:
+    # 获取所有子模块路径
+    cmd = "git config --file .gitmodules --get-regexp path | awk '{ print $2 }'"
+    result = subprocess.run(
+        cmd, shell=True, check=True, stdout=subprocess.PIPE, text=True
+    )
+    submodules = result.stdout.strip().split("\n")
+
+    for submodule in submodules:
+        print(f"Running pre-commit in {submodule}")
+        # 进入子模块目录并执行pre-commit
+        run_cmd("poetry run pre-commit run --all-files", cwd=submodule)
 
 
 def run_tests() -> None:
     # Install requirements by poetry
     run_cmd("poetry install")
 
-    # Run pre-commit tests
-    # run_cmd("poetry run pre-commit autoupdate")
-    # run_cmd("poetry run pre-commit clean")
-    # run_cmd("poetry run pre-commit install")
-    run_cmd("poetry run pre-commit run --all-files")
+    # Run pre-commit tests in the main repository
+    # run_cmd("poetry run pre-commit run --all-files")
 
-    # Generate coverage report --cov=./ --cov-report=xml --cov-report=html -vv
-    # run_cmd("poetry run pytest  --cov=./ --cov-report=xml --cov-report=html -vv")
+    # Optionally run pre-commit in all submodules
+    run_pre_commit_in_submodules()
+
+    # Generate coverage report
+    # run_cmd("poetry run pytest --cov=./ --cov-report=xml --cov-report=html -vv")
 
 
 if __name__ == "__main__":
