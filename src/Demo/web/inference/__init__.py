546b46e0e532b113df817ca55a25b3acf577aa56
atticuszz <1831768457@qq.com>
2024-03-07 18:50:10 +0800

    chore

--- src/Demo/web/inference/__init__.py
+++ src/Demo/web/inference/__init__.py
@@ -8,11 +8,13 @@
 
 import logging
 
+from ..setttings import CameraConfig, DetectorConfig, TrackerConfig
 from .common import ImageFaces
-from .component.camera import Camera, Camera, CameraOpenError
-from .component.detector import Detector, Detector
+from .component.camera import Camera, CameraOpenError
+from .component.detector import Detector
 from .component.drawer import Drawer
 from .component.identifier import Identifier
+from .types import Image
 from .utils.decorator import error_handler
 from .utils.time_tracker import time_tracker
 
@@ -20,29 +22,32 @@ logger = logging.getLogger(__name__)
 
 
 class BoostFace:
-    def __init__(self):
-        self._camera = Camera()
-        self._detector = Detector()
-        self._identifier = Identifier()
+    def __init__(
+        self,
+        camera_config: CameraConfig,
+        detector_config: DetectorConfig,
+        tracker_config: TrackerConfig,
+    ):
+        self._camera = Camera(camera_config)
+        self._detector = Detector(detector_config)
+        self._identifier = Identifier(tracker_config)
         self._draw = Drawer()
 
     @error_handler
-    @time_tracker.track_func
-    def get_result(self) -> ImageFaces:
+    def get_result(self, image: Image | None = None) -> Image:
         """
         :exception CameraOpenError
         :return: Image
         """
-
-        frame: ImageFaces = self._camera.read()
+        if image is None:
+            frame: ImageFaces = self._camera.read()
+        else:
+            frame = ImageFaces(image=image, faces=[])
         detected = self._detector.run_onnx(frame)
         identified = self._identifier.identify(detected)
         draw_on = self._draw.show(identified)
-        return draw_on
+        return draw_on.nd_arr
 
     @error_handler
     def stop_app(self):
         self._identifier.stop_ws_client()
-
-
-onnx_runner = BoostFace()
